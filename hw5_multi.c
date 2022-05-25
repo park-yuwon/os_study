0a7a96144d01a1f0118c2566d88a6077c7c0a823
park-yuwon <77564623+park-yuwon@users.noreply.github.com>
2022-05-25 21:44:58 +0900

    Update hw5_multi.c

--- hw5_multi.c
+++ hw5_multi.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <pthread.h>
+#include <stdbool.h>
 
-#define true 1
-#define false 0
+int CNT = 0;
+pthread_mutex_t mutex;
 
-int glob;
-
+/*
 int cntPrime(int num) {
 	int cnt = 0;
 	int i;
@@ -17,45 +17,65 @@ int cntPrime(int num) {
 	}
 	if (i * i == num) cnt++;
 
-
-	pthread_exit(&glob); // Added
-	
 	if (cnt == 1) return true;
 	else return false;
+}*/
+
+void* partial_prime(void* param) {
+
+	pthread_mutex_lock(&mutex);
+	int range = *(int*)param;
+	printf("thread for range (%d ~ %d) \n", range, range + 99999);
+	int cnt = 0;
+	int i;
+
+	for (i = 1; i * i < range; i++) {
+		if (range % i == 0) cnt++;
+	}
+	if (i * i == range) cnt++;
+	CNT += cnt;
+	printf("The number of prime numbers between %d~%d is %d\n", range, range + 99999, cnt);
+
+	pthread_mutex_unlock(&mutex);
+	pthread_exit(0);
 }
 
+
 int main(int argc, char* argv[]) {
-	int cnt = 0;
-	int total_cnt = 0;
 	int range = atoi(argv[1]);
-	int thread_num = 20;
-	int quantum = range / thread_num;
-	pthread_t thread_id[20];
 
 	struct timespec start, finish;
 	double elapsed;
 
+	// start
 	clock_gettime(CLOCK_MONOTONIC, &start);
-	/* Multi Threading Part Should be here ...*/
 
-	for (int base = 0; base < range; base += quantum) {
-		if (base < 2) continue;
-		cnt = 0;
-		for (int i = 0; i < quantum; i++) {
-			pthread_create(&thread_id[base + i], NULL, cntPrime(base + i), NULL);
-			if (cntPrime(i) == true) cnt++;
-		}
-		printf("The number of prime numbers between %d~%d is %d\n", base, base + quantum, cnt);
-		total_cnt += cnt;
+	// MUTEX lock
+	mutex = PTHREAD_MUTEX_INITIALIZER;
+
+	int num_thread = 0;
+	pthread_t tids[1024];
+	int limit[1024];
+
+	int idx = 0;
+	while (idx < range) {
+		limit[num_thread] = idx;
+		num_thread++;
+		// add logic to create threads
+		pthread_create(&tids[idx], NULL, partial_prime(idx), NULL);
+
+		idx += 100000;
+		if (idx + 1 > range) idx = range;
 	}
-		
-	clock_gettime(CLOCK_MONOTONIC, &finish);
+	// add logic to wait all the threads until they finish the prime number computation
 
+	// add logic to measure the time clock difference
+	clock_gettime(CLOCK_MONOTONIC, &finish);
 	elapsed = (finish.tv_sec - start.tv_sec); // seconds
 	elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0; // nanoseconds
 
 	printf("elapsed time: %f sec \n", elapsed);
-	printf("The number of prime numbers between 1~%d is %d\n", range, total_cnt);
+	printf("The number of prime numbers between 1~%d is %d\n", range, CNT);
 
 	return 0;
 }
